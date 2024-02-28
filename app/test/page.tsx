057068c754a09655d137368c6a808ead71acd839
Mohammad Fahim Uddin <uddin200401@gmail.com>
2024-02-28 15:40:06 +0600

    test: test code

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,15 +3,13 @@ import { useState } from 'react';
 import FormInput from '../components/apply-incentive/FormInput';
 
 const page = () => {
-
-  const [input,setInput] =useState("");
+  const [input, setInput] = useState('');
+  console.log('check');
 
   return (
     <div className="min-h-screen mt-20">
       <FormInput title={'this is my title'} setInput={setInput} />
-      <div>
-        {input}
-      </div>
+      <div>{input}</div>
     </div>
   );
 };
