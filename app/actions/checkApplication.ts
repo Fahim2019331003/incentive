c16373fc04f9d160edcc998b2f026bef46f9f735
Muntasir <Fahim2019331003>
2024-03-18 23:28:46 +0600

    style:frontend changed

---
+++ app/actions/checkApplication.ts
@@ -0,0 +1,13 @@
+//ekhan theke kaj shuru korbo
+
+interface formInput {
+  email?: string;
+  designation;
+}
+
+export default async function checkApplication(formInput) {
+  try {
+  } catch (error: any) {
+    return error.mesage;
+  }
+}
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,15 +1,35 @@
 'use client';
-import { useState } from 'react';
-import FormInput from '../components/apply-incentive/FormInput';
+import axios from 'axios';
 
 const Page = () => {
-  const [input, setInput] = useState('');
-  console.log('check');
+  const data = {
+    department: 'xyhusdbcuhbyz',
+    school: '123sdcsd4',
+    title: 'yusghccasdcdsihs',
+    qIndex: 'Q1',
+    PublishernameAndAddress: 'uysdfghisdcdscdhsbc',
+    volAndDate: '267821asdcd372',
+  };
+
+  const handleClick = async () => {
+    // console.log(data);
+    const res = await axios
+      .post('/api/application', {
+        data,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="min-h-screen mt-20">
-      <FormInput title={'this is my title'} setInput={setInput} />
-      <div>{input}</div>
+      <button type="button" onClick={() => handleClick()}>
+        HEEEEELLLLOOOO
+      </button>
     </div>
   );
 };
